5b5ac6957a3c03b7eb59d34404fadc25ffbd7b59
angelica <angyita15@gmail.com>
2023-05-12 13:16:05 +0200

    aggiunta ciel

--- sketch.js
+++ sketch.js
@@ -7,26 +7,39 @@
 
 **/
 
-function preload() {
-  treeModel = loadAsset("albero.glb");
-}
-
 function setup() {
-  createCanvas3D(windowWidth, windowHeight);
-  
+  createCanvas3D(600, 400);
   //Change background color of the scene
-  background3D("#333333");
+  background3D("#00BCD4");
   
-  hideGrid();
+  var albero = beginGroup();
+    //Change material color to orange
+    diffuse("#3FBD44");
+    //Create a sphere in 0, 0, 0 with radius 1
+    sphere(0, 3.5, 0, 2);
+    diffuse("#3B1A0E");
+    align(TOP);
+     cylinder(0, 0, 0, 0.3, 3.5);
+  endGroup();
+  var lato = 20
+  var numeroAlberi = 5;
+  for (var i=0; i<numeroAlberi; i++)
+    {
+          var x = random(-lato/2 , lato/2);
+          var y = 0;
+          var z = 0;random(-lato/2 , lato/2);
+      
+    var nuovoAlbero=  clone(albero, x, y, z);
+      nuovoAlbero.setScale(random(1,2));
+    }
   
-  var tree = clone(treeModel,0,0,0);
-
   align(BOTTOM);
-  diffuse("grey");
-  box(0, 0, 0, 6, 0.5, 6);
+  box(0,0, 0, 200, 20, 200);
+  
+ 
 }
 
 function draw() {
   // 🔍 Never create objects here, but don't forget to have it in your code:
   // leave empty for the moment
-}
+} 
